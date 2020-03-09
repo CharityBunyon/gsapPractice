e428d16dc125ee9cf786f8149db9f38e04adde05
Charity Bunyon <cseymou5@gmail.com>
2020-03-08 22:21:30 -0500

    Learning React Hooks

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,50 @@
-import React from 'react';
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-sequences */
+import React, { useRef, useEffect } from 'react';
+import { TweenMax, Power3 } from 'gsap';
 import logo from './logo.svg';
 import './App.scss';
 
 function App() {
+  let logoItem = useRef('null');
+  // console.log(logoItem);
+  let textItem = useRef(null);
+
+  useEffect(() => {
+    console.log(logoItem);
+    // logoItem.style.display = 'none';
+    TweenMax.to(
+      logoItem,
+      5,
+      {
+        opacity: 1,
+        y: -20,
+        ease: Power3.easeOut,
+      },
+    );
+    TweenMax.to(
+      textItem,
+      5,
+      {
+        opacity: 1,
+        y: -20,
+        ease: Power3.easeOut,
+        delay: 0.2,
+      },
+    );
+  }, []);
+
+  // Learning about hooks: UseEffect and UseRef
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <img ref={ (el) => { logoItem = el; }}
+        src={logo} className="App-logo" alt="logo" />
+        <p
+        ref={ (el) => { textItem = el; }}>
+
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -19,8 +56,6 @@ function App() {
           Learn React
         </a>
       </header>
-
-      <button className="btn btn-danger">Bootstrap Button</button>
     </div>
   );
 }
